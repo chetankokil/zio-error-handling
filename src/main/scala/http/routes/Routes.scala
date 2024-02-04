415f0683ede74accd74412e1b697174b543fce52
chetankokil <All189@#>
2024-02-04 08:56:35 -0600

    Added get person endpoint

--- src/main/scala/http/routes/Routes.scala
+++ src/main/scala/http/routes/Routes.scala
@@ -1,62 +1,70 @@
 package http.routes
 
-  import sttp.model.StatusCode
-  import sttp.tapir.EndpointOutput.OneOf
-  import io.circe.generic.auto.*
-  import sttp.tapir.json.circe.*
-  import sttp.tapir.PublicEndpoint
-  import sttp.tapir.generic.auto.*
-  import zio.*
-  import zio.interop.catz.*
-  import org.http4s.HttpApp
-  import org.http4s.Request
-  import org.http4s.Response
-  import sttp.tapir.ztapir.*
-  import sttp.tapir.server.http4s.*
-  import sttp.tapir.server.http4s.ztapir.*
-  import sttp.tapir.ztapir.*
-  import sttp.tapir.json.circe.*
-  import sttp.tapir.generic.auto.*
-  import org.http4s.HttpRoutes
-  import http.ErrorInfo
-  import http.models.Person
-  import http.ErrorInfo.{ BadRequest, InternalServerError, NotFound }
-  import services.PersonService
-  import services.PersonServiceLive
+import sttp.model.StatusCode
+import sttp.tapir.EndpointOutput.OneOf
+import io.circe.generic.auto.*
+import sttp.tapir.json.circe.*
+import sttp.tapir.PublicEndpoint
+import sttp.tapir.generic.auto.*
+import zio.*
+import zio.interop.catz.*
+import org.http4s.HttpApp
+import org.http4s.Request
+import org.http4s.Response
+import sttp.tapir.ztapir.*
+import sttp.tapir.server.http4s.*
+import sttp.tapir.server.http4s.ztapir.*
+import sttp.tapir.ztapir.*
+import sttp.tapir.json.circe.*
+import sttp.tapir.generic.auto.*
+import org.http4s.HttpRoutes
+import http.ErrorInfo
+import http.models.Person
+import http.ErrorInfo.{ BadRequest, InternalServerError, NotFound }
+import services.PersonService
+import services.PersonServiceLive
 
+object Routes:
+  val httpErrors: OneOf[ErrorInfo, ErrorInfo] = oneOf[ErrorInfo](
+    oneOfVariant(StatusCode.InternalServerError, jsonBody[InternalServerError]),
+    oneOfVariant(StatusCode.BadRequest, jsonBody[BadRequest]),
+    oneOfVariant(StatusCode.NotFound, jsonBody[NotFound])
+  )
 
-  object Routes:
-    val httpErrors: OneOf[ErrorInfo, ErrorInfo] = oneOf[ErrorInfo](
-      oneOfVariant(StatusCode.InternalServerError, jsonBody[InternalServerError]),
-      oneOfVariant(StatusCode.BadRequest, jsonBody[BadRequest]),
-      oneOfVariant(StatusCode.NotFound, jsonBody[NotFound])
-    )    
+  val createPersonEndpoint: PublicEndpoint[Person, ErrorInfo, Long, Any] = endpoint.post
+    .in("person")
+    .in(jsonBody[Person])
+    .out(plainBody[Long])
+    .errorOut(httpErrors)
 
-    val createPersonEndpoint: PublicEndpoint[Person, ErrorInfo, Long, Any] = endpoint.post
-      .in("person")
-      .in(jsonBody[Person])
-      .out(plainBody[Long])
-      .errorOut(httpErrors)
+  val getPersonEndpoint = endpoint.get
+    .in("person" / path[Long]("id"))
+    .out(jsonBody[Option[Person]])
+    .errorOut(httpErrors)
 
-    val createPersonServer: ZServerEndpoint[PersonService, Any] = createPersonEndpoint.zServerLogic { person =>
-      PersonService.create(person).mapError {
-        case e: Exception => {
-          println(e)
-          InternalServerError("Internal Server Error")
-        }
-        case _ => InternalServerError("Internal Server Error")
-      }
+  val createPersonServer: ZServerEndpoint[PersonService, Any] = createPersonEndpoint.zServerLogic { person =>
+    PersonService.create(person).mapError {
+      case e: Exception =>
+        println(e)
+        InternalServerError("Internal Server Error")
+      case _ => InternalServerError("Internal Server Error")
     }
+  }
 
-    val helloWorldEndpoint: PublicEndpoint[Unit, ErrorInfo, String, Any] = endpoint.get
-      .in("hello")
-      .out(stringBody)
-      .errorOut(httpErrors)
+  val getPersonServer = getPersonEndpoint.zServerLogic { (id: Long) =>
+    PersonService.read(id).mapError(_ => InternalServerError(s"Person with Id $id not found"))
+  }
 
-    val helloworldServer: ZServerEndpoint[PersonService, Any] = helloWorldEndpoint.zServerLogic { _ =>
-      ZIO.succeed("Hello, World!")
-    }
+  val helloWorldEndpoint: PublicEndpoint[Unit, ErrorInfo, String, Any] = endpoint.get
+    .in("hello")
+    .out(stringBody)
+    .errorOut(httpErrors)
 
-    val allEndpoints : List[ZServerEndpoint[PersonService, Any]] = List(createPersonServer, helloworldServer)
+  val helloworldServer: ZServerEndpoint[PersonService, Any] = helloWorldEndpoint.zServerLogic { _ =>
+    ZIO.succeed("Hello, World!")
+  }
 
-    val routes = ZHttp4sServerInterpreter().from(allEndpoints).toRoutes
+  val allEndpoints: List[ZServerEndpoint[PersonService, Any]] =
+    List(createPersonServer, helloworldServer, getPersonServer)
+
+  val routes = ZHttp4sServerInterpreter().from(allEndpoints).toRoutes
--- src/main/scala/services/PersonService.scala
+++ src/main/scala/services/PersonService.scala
@@ -2,7 +2,7 @@ package services
 
 import http.models.Person
 import domain.repository.PersonRepository
-import zio.{Task, URLayer, ZLayer, ZIO }
+import zio.{ Task, URLayer, ZIO, ZLayer }
 import domain.repository.PersonRepositoryLive
 import zio.interop.catz.*
 import zio.interop.catz.implicits.*
@@ -16,18 +16,19 @@ trait PersonService:
 
 object PersonService:
   def create(entity: Person): ZIO[PersonService, Throwable, Long] = ZIO.serviceWithZIO[PersonService](_.create(entity))
-  def read(id: Long): Task[Option[Person]] = ???
-  def update(entity: Person): Task[Unit] = ???
-  def delete(id: Long): Task[Unit] = ???
-  def list(): Task[List[Person]] = ???
+  def read(id: Long): ZIO[PersonService, Throwable, Option[Person]] = ZIO.serviceWithZIO[PersonService](_.read(id))
+  def update(entity: Person): Task[Unit]                            = ???
+  def delete(id: Long): Task[Unit]                                  = ???
+  def list(): Task[List[Person]]                                    = ???
 
 case class PersonServiceLive(repo: PersonRepository) extends PersonService:
-    def create(entity: Person): Task[Long] = repo.create(entity)
-    def read(id: Long): Task[Option[Person]] = repo.read(id)
-    def update(entity: Person): Task[Unit] = repo.update(entity)
-    def delete(id: Long): Task[Unit] = repo.delete(id)
-    def list(): Task[List[Person]] = repo.list()
+  def create(entity: Person): Task[Long]   = repo.create(entity)
+  def read(id: Long): Task[Option[Person]] = repo.read(id)
+  def update(entity: Person): Task[Unit]   = repo.update(entity)
+  def delete(id: Long): Task[Unit]         = repo.delete(id)
+  def list(): Task[List[Person]]           = repo.list()
 
 object PersonServiceLive:
-    val layer: URLayer[PersonRepository, PersonService] = 
-        ZLayer.fromFunction(PersonServiceLive(_))
+  val layer: URLayer[PersonRepository, PersonService] =
+    ZLayer.fromFunction(PersonServiceLive(_))
+
